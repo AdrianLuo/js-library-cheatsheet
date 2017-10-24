21e05074e5cd31a903ea60c2c19b9a55bd46bcff
Adrian Luo <adrian.luo@goradii.com>
2017-10-24 12:37:39 -0400

    Sticky scroll

--- js-cheatsheet.js
+++ js-cheatsheet.js
@@ -7,3 +7,19 @@ $(document).ready(function(){ //insert function });
 $(".class_name").each(function (i) { 
 	$(this).attr('tabindex', i + 1); 
 });
+
+// 1.0 - SM & LG desktop sticky navigation
+var window_width = $(window).width();
+$(window).scroll(function() {
+
+	var window_top_scroll = $(window).scrollTop();
+	
+	if(window_width > 768){ 
+		if(window_top_scroll > 100){
+			$('#site-header').addClass('sticky-nav');
+		} else { 
+			$('#site-header').removeClass('sticky-nav');
+		}	
+	}
+	
+});
